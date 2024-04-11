6a83a47d7d81d43c47ff7fa3d434549f1fdaf230
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-04-10 21:57:55 -0500

    CM

--- Eurocargasite/mainsite/urls.py
+++ Eurocargasite/mainsite/urls.py
@@ -11,8 +11,12 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("about/", views.about, name="about"),
-    
+    path("air_services/", views.air_services, name="air_services"),
+    path("land_services/", views.land_services, name="land_services"),
+    path("other_services/", views.other_services, name="other_services"),
+    path("sea_services/", views.sea_services, name="sea_services"),
+    path("team/", views.team, name="team"),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += staticfiles_urlpatterns()
--- Eurocargasite/mainsite/views.py
+++ Eurocargasite/mainsite/views.py
@@ -1,17 +1,57 @@
 from django.shortcuts import render
 
+
 def index(request):
 
-    
     context = {
         "key": 0,
     }
-    return render(request, 'mainsite/index.html', context)
+    return render(request, "mainsite/index.html", context)
+
 
 def about(request):
 
-    
     context = {
         "key": 0,
     }
-    return render(request, 'mainsite/about.html', context)
+    return render(request, "mainsite/about.html", context)
+
+
+def land_services(request):
+
+    context = {
+        "key": 0,
+    }
+    return render(request, "mainsite/land_services.html", context)
+
+
+def air_services(request):
+
+    context = {
+        "key": 0,
+    }
+    return render(request, "mainsite/air_services.html", context)
+
+
+def other_services(request):
+
+    context = {
+        "key": 0,
+    }
+    return render(request, "mainsite/other_services.html", context)
+
+
+def sea_services(request):
+
+    context = {
+        "key": 0,
+    }
+    return render(request, "mainsite/sea_services.html", context)
+
+
+def team(request):
+
+    context = {
+        "key": 0,
+    }
+    return render(request, "mainsite/team.html", context)
